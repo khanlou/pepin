e339cdd4c8f79043fa06e71c0f7f3d500062ab77
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-01-17 14:06:34 -0500

    add amount tests

---
+++ test/amount.js
@@ -0,0 +1,61 @@
+var Unit = require('../domain/unit');
+var Amount = require('../domain/amount');
+var assert = require('assert');
+
+describe("amount", function() {
+  it("amounts should be constructed", function() {
+    var amount = new Amount(1, Unit.unitFromName('cup'), 'flour');
+    
+    assert.equal(amount.quantity, 1);
+    assert.equal(amount.unit.name, 'cup');
+    assert.equal(amount.ingredientName, 'flour');
+  });
+  
+  it("amounts should parse 'a'", function() {
+    var amount = new Amount('a', Unit.unitFromName('cup'), 'flour');
+    
+    assert.equal(amount.quantity, 1);
+    assert.equal(amount.unit.name, 'cup');
+    assert.equal(amount.ingredientName, 'flour');
+  });
+
+  it("amounts should parse 'an'", function() {
+    var amount = new Amount('an', Unit.unitFromName('ounce'), 'flour');
+    
+    assert.equal(amount.quantity, 1);
+    assert.equal(amount.unit.name, 'ounce');
+    assert.equal(amount.ingredientName, 'flour');
+  });
+  
+  it("amounts should parse fractions", function() {
+    var amount = new Amount('3/4', Unit.unitFromName('cup'), 'flour');
+    
+    assert.equal(amount.quantity, 0.75);
+    assert.equal(amount.unit.name, 'cup');
+    assert.equal(amount.ingredientName, 'flour');
+  });
+  
+  it("amounts should parse decimals", function() {
+    var amount = new Amount('0.25', Unit.unitFromName('cup'), 'flour');
+    
+    assert.equal(amount.quantity, 0.25);
+    assert.equal(amount.unit.name, 'cup');
+    assert.equal(amount.ingredientName, 'flour');
+  });
+  
+  it("should scale amounts", function() {
+    var amount = new Amount(1, Unit.unitFromName('cup'), 'flour');
+    var scaled = amount.amountByScaling(3)
+    
+    assert.equal(scaled.quantity, 3)
+    assert.equal(scaled.unit.name, 'cup')
+    assert.equal(scaled.ingredientName, 'flour')
+  });
+  
+  
+});
+
+//test plurals
+//test reducing
+//test scaling
+//test different patterns
