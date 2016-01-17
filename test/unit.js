04b8580d1f861cd48b363931b2087dbc6e6a9162
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-01-17 14:01:43 -0500

    add tests for unit

---
+++ test/unit.js
@@ -0,0 +1,21 @@
+var Unit = require('../domain/unit');
+var assert = require('assert');
+
+describe("unit", function() {
+  it("should find units by their names", function() {
+    var unit = Unit.unitFromName('cup');
+    assert.equal(unit.name, 'cup');
+  });  
+  
+  it("should find units by their plurals", function() {
+    var unit = Unit.unitFromName('cups');
+    assert.equal(unit.name, 'cup');
+  });  
+  
+  it("should find units by their alternate names", function() {
+    var unit = Unit.unitFromName('c');
+    assert.equal(unit.name, 'cup');
+  });  
+  
+  
+});
