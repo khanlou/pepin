f3770ffabeafd2c06d150b118a6d36b796f8dd4c
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-01-17 14:23:32 -0500

    add amount presenter tests

---
+++ test/amount_presenter.js
@@ -0,0 +1,34 @@
+var AmountPresenter = require('../domain/presenters').AmountPresenter;
+var Pattern = require('../domain/pattern');
+var Amount = require('../domain/amount');
+var Unit = require('../domain/unit');
+var assert = require('assert');
+
+describe("amount presenter", function() {
+  var pattern = new Pattern("{quantity} {unit} of {ingredient}")
+  
+  it("should present amounts", function() {
+    var amount = new Amount(1, Unit.unitFromName('cup'), 'flour');
+    var amountPresenter = new AmountPresenter(pattern, amount);
+    assert(amountPresenter.stringForDisplay = "1 cup of flour");
+  });
+  
+  it("should pluralize amounts", function() {
+    var amount = new Amount(2, Unit.unitFromName('cup'), 'flour');
+    var amountPresenter = new AmountPresenter(pattern, amount);
+    assert(amountPresenter.stringForDisplay = "2 cups of flour");
+  });
+  
+  it("should reduce amounts", function() {
+    var amount = new Amount(3, Unit.unitFromName('teaspoons'), 'flour');
+    var amountPresenter = new AmountPresenter(pattern, amount);
+    assert(amountPresenter.stringForDisplay = "1 tablespoon of flour");
+  });
+  
+  it("should reduce and pluralize amounts", function() {
+    var amount = new Amount(6, Unit.unitFromName('teaspoons'), 'flour');
+    var amountPresenter = new AmountPresenter(pattern, amount);
+    assert(amountPresenter.stringForDisplay = "2 tablespoons of flour");
+  });
+
+});
