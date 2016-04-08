2a8401a1a005f267c83aa10f7ff164b2de7312db
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-04-08 12:32:04 -0400

    add a parser test for things with no units

--- test/parser.js
+++ test/parser.js
@@ -18,6 +18,15 @@ describe("parser", function() {
     assert.equal(ingredientLine.ingredient.name, 'egg')
   });
   
+  it("should parse ingredients with no unit", function() {
+    var parser = new IngredientParser("2 eggs");
+    var ingredientLine = parser.ingredientLine;
+    console.log(ingredientLine)
+    assert.equal(ingredientLine.amount.quantity, 2)
+    assert.equal(ingredientLine.amount.unit.name, '')
+    assert.equal(ingredientLine.ingredient.name, 'eggs')
+  });
+  
 
   it("should parse ingredient lines with 'of'", function() {
     var parser = new IngredientParser("1 cup of flour");
@@ -55,5 +64,11 @@ describe("parser", function() {
     assert.equal(amountString, 'vegetable oil')
   });
   
-  
+  it("should parse a complex thing", function() {
+    var parser = new IngredientParser("2 eggs, cold, lightly beaten in a separate bowl")
+    var ingredientLine = parser.ingredientLine;
+    assert.equal(ingredientLine.amount.quantity, 2)
+    assert.equal(ingredientLine.amount.unit.name, '')
+    assert.equal(ingredientLine.ingredient.name, 'eggs')
+  })
 });
